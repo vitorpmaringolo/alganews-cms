edd49620991f7874c226ad91214b75e8366fa775
Vitor Palhares Maringolo <vitor.pmaringolo@gmail.com>
2021-08-09 22:33:20 -0300

    4.42. Documentando o CircleChart

---
+++ src/stories/NoData.stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/react';
+
+import NoData, { NoDataProps } from '../components/NoData/NoData';
+
+export default {
+  title: 'Example/NoData',
+  component: NoData
+} as Meta;
+
+const Template: Story<NoDataProps> = (args) => <NoData {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const FixedHeight = Template.bind({})
+Default.args = {
+    height: 240
+}
