eed2ba18aea43c45576e6de069aa55b3d9d3de8e
Vitor Palhares Maringolo <vitor.pmaringolo@gmail.com>
2021-08-12 22:19:45 -0300

    4.48. Desafio - SessionController

---
+++ src/stories/ImageUpload.stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react';
+
+import ImageUpload, { ImageUploadProps } from '../components/ImageUpload';
+
+export default {
+  title: 'Example/ImageUpload',
+  component: ImageUpload
+} as Meta;
+
+const Template: Story<ImageUploadProps> = (args) => <ImageUpload {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+    label: 'Thumbnail do post'
+}
---
+++ src/stories/SessionController.stories.tsx
@@ -0,0 +1,24 @@
+import { Story, Meta } from '@storybook/react';
+
+import SessionController, { SessionControllerProps } from '../components/SessionController';
+
+export default {
+  title: 'Example/SessionController',
+  component: SessionController,
+  argTypes: {
+    onLogout: {
+        action: 'logout'
+    }
+  },
+} as Meta;
+
+const Template: Story<SessionControllerProps> = (args) => 
+    <div>
+        <SessionController {...args} />
+    </div>  
+
+export const Default = Template.bind({})
+Default.args = {
+    name: 'Daniel Bonifácio',
+    description: 'editor há muito tempo'
+}
